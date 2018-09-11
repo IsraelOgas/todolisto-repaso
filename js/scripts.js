fdaf48a4d5d0099b5a2c4747ccd0b4ff514b3d3a
saintJour <matiasfigueroasj@gmail.com>
2018-09-11 12:49:19 -0300

    punto 3 terminado

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $.get(`${dominio}tareas/`, function (tareas) {
     console.log(tareas);
 
     for (const tarea of tareas) {
-        $('#tareas').append('<tr> <th scope="row" class="id">' + tarea.id + '</th> <td class="titulo"> <a class="detalle-tarea" href="' + dominio + 'tareas/' + tarea.id + '">' + tarea.titulo + '</a></td><td class="estado">' + tarea.nombre_estado + '</td> </tr>')
+        $('#tareas').append('<tr id="' + tarea.id + '"> <th scope="row" class="id">' + tarea.id + '</th> <td class="titulo"> <a class="detalle-tarea" href="' + dominio + 'tareas/' + tarea.id + '">' + tarea.titulo + '</a></td><td class="estado">' + tarea.nombre_estado + '</td> </tr>')
     }
 }).fail(function (x) {
     console.log("Error en la llamada Ajax :(");
@@ -19,7 +19,7 @@ $('#form-tarea').submit(function (e) {
     // var len_description = $('#descripcion_id').val().length;
 
     $.post('http://localhost:8000/tareas/', $('#form-tarea').serialize(), function (tarea) {
-        $('#tareas').append('<tr> <th scope="row" class="id">' + tarea.id + '</th> <td class="titulo"> <a class="detalle-tarea" href="' + dominio + 'tareas/' + tarea.id + '">' + tarea.titulo + '</a></td><td class="estado">' + tarea.nombre_estado + '</td> </tr>')
+        $('#tareas').append('<tr id="' + tarea.id + '"> <th scope="row" class="id">' + tarea.id + '</th> <td class="titulo"> <a class="detalle-tarea" href="' + dominio + 'tareas/' + tarea.id + '">' + tarea.titulo + '</a></td><td class="estado">' + tarea.nombre_estado + '</td> </tr>')
     });
 });
 
@@ -70,7 +70,11 @@ $(function() {
 });
 */
 
-/* [GET] Mostrar detalles de una TAREA en especifico  */
+/* 
+[GET] Mostrar detalles de una TAREA en especifico  
+[PUT] Se actualiza el estado de una tarea
+Se actualiza en la tabla
+*/
 $('.table').on('click', '.detalle-tarea', function (e) {
     e.preventDefault();
     // $(".section-tareas").empty();
@@ -78,20 +82,47 @@ $('.table').on('click', '.detalle-tarea', function (e) {
     $.get(e.currentTarget.href, function (data) {
         $('.section-tareas .titulo').html(data.id + ' ' + data.titulo);
         $('.section-tareas .descripcion').html(data.descripcion);
-
+        console.log(data);
         if (data.estado == 0) {
-            $('.section-tareas button').html('Pasar a estado En Proceso').show();
-
+            $('.section-tareas button').html('Pasar a estado En Proceso').show().off();
         } else if (data.estado == 1) {
-            $('.section-tareas button').html('Pasar a estado Terminada').show();
+            $('.section-tareas button').html('Pasar a estado Terminada').show().off();
         } else {
             $('.section-tareas button').hide();
         }
-        console.log(data);
+        $('.section-tareas button').click(function(){
+            $.get(e.currentTarget.href, function (data){
+                if (data.estado == 0) {
+                    $.ajax({
+                      url: dominio + 'tareas/' + data.id + '/',
+                      type: 'PUT',
+                      data: 'estado=1&titulo=' + data.titulo + '&descripcion=' + data.descripcion
+                    }).done(function(){
+                        console.log('ok');
+                    }).fail(function(){
+                        console.log('fail');
+                    });
+                    $('.section-tareas button').html('Pasar a estado Terminada');
+                    $("#tareas #" + data.id + " .estado").html("En Proceso");   
+                } else {
+                    $.ajax({
+                      url: dominio + 'tareas/' + data.id + '/',
+                      type: 'PUT',
+                      data: 'estado=2&titulo=' + data.titulo + '&descripcion=' + data.descripcion
+                    }).done(function(){
+                        console.log('ok');
+                    }).fail(function(){
+                        console.log('fail');
+                    });
+                    $('.section-tareas button').hide();
+                    $("#tareas #" + data.id + " .estado").html("Terminada");
+                }
+            });
+        });
     });
 });
 
-// Activar botón Crear Tarea cuando se cumplan condiciones en formulario
+/* Activar botón Crear Tarea cuando se cumplan condiciones en formulario */
 
 $('#boton_crear_tarea').prop('disabled', true);
 
@@ -102,4 +133,8 @@ $('#titulo_id, #descripcion_id').keyup(function(){
     else{
         $('#boton_crear_tarea').prop('disabled', true);
     }
-})
+});
+
+
+
+
