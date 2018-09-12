4745550d0ace2497194f6305badef2bdb04bb3ad
Israel Ogas <ogasvega.israel@gmail.com>
2018-09-11 21:35:19 -0300

    feat: añadido el punto (6) [barra de búsqueda]

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var dominio = 'http://localhost:8000/';
 
 /* [GET] Obtener listado de tareas */
 $.get(`${dominio}tareas/`, function (tareas) {
-    console.log(tareas);
+    // console.log(tareas);
 
     for (const tarea of tareas) {
         $('#tareas').append('<tr id="' + tarea.id + '"> <th scope="row" class="id">' + tarea.id + '</th> <td class="titulo"> <a class="detalle-tarea" href="' + dominio + 'tareas/' + tarea.id + '">' + tarea.titulo + '</a></td><td class="estado">' + tarea.nombre_estado + '</td> </tr>')
@@ -15,8 +15,6 @@ $.get(`${dominio}tareas/`, function (tareas) {
 /* [POST] Añadir nuevas tareas */
 $('#form-tarea').submit(function (e) {
     e.preventDefault();
-    // var len_title = $('#titulo_id').val().length;
-    // var len_description = $('#descripcion_id').val().length;
 
     $.post('http://localhost:8000/tareas/', $('#form-tarea').serialize(), function (tarea) {
         $('#tareas').append('<tr id="' + tarea.id + '"> <th scope="row" class="id">' + tarea.id + '</th> <td class="titulo"> <a class="detalle-tarea" href="' + dominio + 'tareas/' + tarea.id + '">' + tarea.titulo + '</a></td><td class="estado">' + tarea.nombre_estado + '</td> </tr>')
@@ -46,30 +44,6 @@ $('#descripcion_id').keyup(function () {
     $('#charNum').text('(' + len + ')');
 });
 
-
-// $('input').on('blur keyup', function() {
-//     var len_title = $('#titulo_id').val().length;
-//     var len_description = $('#descripcion_id').val().length;
-
-//     if (len_title >= 5 && len_description >= 10) {
-//         $('.add-tarea').prop('disabled', false);  
-//     } else {
-//         $('.add-tarea').prop('disabled', 'disabled');
-//     }
-// });
-
-/*
-$(function() {
-    $('.add-tarea').prop('disabled', true);
-
-    $('#titulo_id').keyup(function () {
-        if($(this).val() > 5) {
-            $('.add-tarea').prop('disabled', false);
-        }
-    });
-});
-*/
-
 /* 
 [GET] Mostrar detalles de una TAREA en especifico  
 [PUT] Se actualiza el estado de una tarea
@@ -78,15 +52,14 @@ Se actualiza en la tabla
 $('.table').on('click', '.detalle-tarea', function (e) {
     e.preventDefault();
     // $(".section-tareas").empty();
-    console.log(e.currentTarget.href);
     $.get(e.currentTarget.href, function (data) {
         $('.section-tareas .titulo').html(data.id + ' ' + data.titulo);
         $('.section-tareas .descripcion').html(data.descripcion);
-        console.log(data);
+        // console.log(data);
         if (data.estado == 0) {
-            $('.section-tareas button').html('Pasar a estado En Proceso').show().off();
+            $('.section-tareas button').text('Pasar a estado En Proceso').show().off();
         } else if (data.estado == 1) {
-            $('.section-tareas button').html('Pasar a estado Terminada').show().off();
+            $('.section-tareas button').text('Pasar a estado Terminada').show().off();
         } else {
             $('.section-tareas button').hide();
         }
@@ -123,7 +96,6 @@ $('.table').on('click', '.detalle-tarea', function (e) {
 });
 
 /* Activar botón Crear Tarea cuando se cumplan condiciones en formulario */
-
 $('#boton_crear_tarea').prop('disabled', true);
 
 $('#titulo_id, #descripcion_id').keyup(function(){
@@ -135,6 +107,22 @@ $('#titulo_id, #descripcion_id').keyup(function(){
     }
 });
 
+/* Filtro de búsqueda de tareas por [TITULO] */
+$('input[type=search]').keyup(function() {
+    var input = $('input[type=search]');
+    var filter = input.val().toLowerCase()
+    var tr = $('tbody tr')
 
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName('a')[0]
+      if (td) {
+        if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      } 
+    }
+});
 
 
